0f62870c0f90aa80a9041518f54668f4078bd3c4
Aamir Suleman <asuleman@cai.local>
2022-04-20 19:21:53 -0400

    added any type

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import styles from "./layout.module.css";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
   return <div className={styles.container}>{children}</div>;
 }
