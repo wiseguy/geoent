f2294768f02d6d8b5c767bad7c93f4f5d8489e80
Aamir Suleman <asuleman@cai.local>
2022-04-20 18:28:17 -0400

    added mapbox

--- services/AppService.ts
+++ services/AppService.ts
@@ -1,7 +1,4 @@
 //const mapboxToken = ;
 export function getMapboxToken() {
-  console.log("mapboxToken");
-  console.log(process.env.NEXT_PUBLIC_MAPBOX_TOKEN);
-
   return process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 }
