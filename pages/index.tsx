37126f1a8109edf763eb1f83e03821496d170756
wiseguy <aamirsul@gmail.com>
2022-09-22 19:17:38 -0400

    converted to geoent

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,72 +19,32 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Aamir Sulemans personal testing ground!
+          GeoEnt
         </h1>
 
         <p className={styles.description}>
-          Each page is a experimental piece of work. Check them out!
+        We Make Things Happen
         </p>
 
         <div className={styles.grid}>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Qila Direction &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Mapbox &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Firebase &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-
-          <Link href="/ar">
-            <div className={styles.card}>
-              <h2>AR &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Planetscale &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Prisma &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Tilling Dynamic &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Three.js &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
-          <Link href="/mapbox">
-            <div className={styles.card}>
-              <h2>Machine Learning &rarr;</h2>
-              <p>Find Mapbox testing paages</p>
-            </div>
-          </Link>
+          <div>Aamir Suleman</div>
         </div>
+        <div className={styles.grid}>
+          <div>Ashburn, VA</div>
+        </div>
+
+        <div className={styles.grid}>
+        <a href="tel:+1-202-560-3930">+1-202-560-3930</a>
+        </div>
+
+        <div className={styles.grid}>
+        GeoEnt has entered the supply market in the new era. It is our goal to provide our customers with quality products at competitive prices, backed by the highest level of commitment and service available in the industry.
+
+      GeoEnt realizes that our ultimate success is dependent on client satisfaction. Here at GeoEnt we provide a broad range of electrical products for construction, commercial, industrial, and residential markets. We are committed to total client satisfaction by
+      meeting or exceeding expectations for quality, delivery, and friendly knowledgeable customer service. Our vision is to be your number one source for all your electrical supply and installation needs. We are faster, easier, and more profitable for you to do business with than our competitors. Every aspect of our on going business strategies will be aimed at accomplishing this vision. We can provide a solution by taking up projects on a turnkey installation contract basis giving you a price advantage by saving you substantial money and procuring the exact quantity.
+        </div>
+
+       
       </main>
 
       <footer className={styles.footer}></footer>
