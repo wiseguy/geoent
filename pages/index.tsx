79300274cfce5175ec4ca4c3743d3ab6e48e2c02
wiseguy <aamirsul@gmail.com>
2022-09-22 22:03:20 -0400

    geoent update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import logo from '../public/geoent-logo.png'
 
 const Home: NextPage = () => {
   const x = 1;
@@ -18,12 +19,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           GeoEnt
-        </h1>
+        
+        </h1> */}
+        <Image src={logo} height='30px' alt='GeoEnt Logo' layout='fixed'/>
 
         <p className={styles.description}>
-        We Make Things Happen
+        Experts in fulfilling international Electrical Supply needs
         </p>
 
         <div className={styles.grid}>
@@ -39,13 +42,12 @@ const Home: NextPage = () => {
         <div className={styles.grid}>
           <a href="mailto:aamir.suleman@geoent.com">aamir.suleman@geoent.com</a>
         </div>
-
+      <br/>
         <div className={styles.grid}>
-        GeoEnt has entered the supply market in the new era. It is our goal to provide our customers with quality products at competitive prices, backed by the highest level of commitment and service available in the industry.
+        GeoEnt is a service-oriented export trading company delivering international companies electrical supplies for their markets.
 
-      GeoEnt realizes that our ultimate success is dependent on client satisfaction. Here at GeoEnt we provide a broad range of electrical products for construction, commercial, industrial, and residential markets. We are committed to total client satisfaction by
-      meeting or exceeding expectations for quality, delivery, and friendly knowledgeable customer service. Our vision is to be your number one source for all your electrical supply and installation needs. We are faster, easier, and more profitable for you to do business with than our competitors. Every aspect of our on going business strategies will be aimed at accomplishing this vision. We can provide a solution by taking up projects on a turnkey installation contract basis giving you a price advantage by saving you substantial money and procuring the exact quantity.
-        </div>
+Through comprehensive business relationships worldwide, GeoEnt has developed unparalleled networking capabilities for profitable international business. These established associations are invaluable assets for the promotion of trade and international business.
+   </div>
 
        
       </main>
